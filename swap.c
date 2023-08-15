9c449c9bab2dbc558b29a5942f2f763c1ec00ebf
oaoba <error@gmail.com>
2023-08-15 14:04:20 +0900

    listno

--- swap.c
+++ swap.c
@@ -1,19 +1,62 @@
 #include "push_swap.h"
 #include <libc.h>
 
-void swap(int *a, int *b)
+void swap(t_list **stack)
 {
-	int tmp;
+	t_list *head = *stack;
+	t_list *next = (*stack)->next;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	int tmp_data = head->data;
+	head->data = next->data;
+	next->data = tmp_data;
+	/*
+	 * swapで考えるとわかりやすい。。
+	 * 　tmp = *a;   //tmp_data = head->data;
+	  	 *a = *b;   //head->data = next->data;
+		 *b = tmp;  //next->data = tmp_data;
+	 */
+	int tmp_index = head->index;
+	head->index = next->index;
+	next->index = tmp_index;
+}
+
+void printList(t_list *node)
+{
+	while (node)
+	{
+		printf("%d, %d->", node->data, node->index);
+		node = node->next;
+	}
+	printf("NULL\n");
 }
 
 int main()
 {
-	int a = 42;
-	int b = 24;
-	swap(&a, &b);
-	printf("%d %d\n", a, b);
+	t_list *node1 = malloc(sizeof(t_list));
+	node1->data = 10;
+	node1->index = 1;
+
+	t_list *node2 = malloc(sizeof(t_list));
+	node2->data = 20;
+	node2->index = 2;
+
+	t_list *node3 = malloc(sizeof(t_list));
+	node3->data = 30;
+	node3->index = 3;
+
+	node1->next = node2;
+	node2->next = NULL;
+	node3->next = NULL;
+
+	printf("Original list\n");
+	printList(node1);
+	swap(&node1);
+	printf("After swapping\n");
+	printList(node1);
+
+	free(node1);
+	free(node2);
+//	free(node3);
+
+	return 0;
 }
