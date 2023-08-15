e6fe16ec2f7a5e13e1321103511d83ca93093f47
oaoba <hacchi0803.rera1126@gmail.com>
2023-08-15 17:49:56 +0900

    リストのなかの最小値を求める関数を作成

---
+++ src/min_of_list.c
@@ -0,0 +1,38 @@
+#include "../push_swap.h"
+
+int min_of_list(t_list *head)
+{
+	int min_value;
+	t_list *current;
+
+	if (!head)
+		return 0;
+	min_value = head->data;
+	current = head->next;
+
+	while (current)
+	{
+		if (current->data < min_value)
+			min_value = current->data;
+		current = current->next;
+	}
+	return min_value;
+}
+//
+//int main()
+//{
+//	t_list node1 = {0, NULL};
+//	t_list node2 = {2, NULL};
+//	t_list node3 = {8, NULL};
+//	t_list node4 = {1, NULL};
+//
+//	node1.next = &node2;
+//	node2.next = &node3;
+//	node3.next = &node4;
+//
+//	int min_value = min_of_list(&node1);
+//	if (min_value == INT_MAX)
+//		printf("min_value is too big");
+//	else
+//		printf("the min of list is : %d\n", min_value);
+//}
---
+++ src/sort_size3.c
@@ -0,0 +1,9 @@
+#include "../push_swap.h"
+
+void sort_size3(t_list **stack_a)
+{
+	t_list	*head;
+	head = *stack_a;
+
+	while ()
+}
