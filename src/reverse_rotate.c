c7582a2e08b477f36b93eec032249c6d115c301e
oaoba <hacchi0803.rera1126@gmail.com>
2023-08-17 13:37:06 +0900

    reverse_rotate 作成。少しコツがいる書き方だった

---
+++ src/reverse_rotate.c
@@ -0,0 +1,40 @@
+#include "../push_swap.h"
+
+void reverse_rotate(t_list **stack)
+{
+	t_list *head;
+	t_list *end;
+
+	head = *stack;
+	end = ft_lstlast(stack);
+	while (head)
+	{
+		if (head->next->next == NULL)
+		{
+			head->next = NULL;
+			break;
+		}
+		head = head->next;
+	}
+	end->next = *stack;
+	*stack = end;
+}
+
+void rra(t_list **stack_a)
+{
+	reverse_rotate(stack_a);
+	ft_putendl_fd("rra", 1);
+}
+
+void rrb(t_list **stack_b)
+{
+	reverse_rotate(stack_b);
+	ft_putendl_fd("rrb", 1);
+}
+
+void rrr(t_list **stack_a, t_list **stack_b)
+{
+	reverse_rotate(stack_a);
+	reverse_rotate(stack_b);
+	ft_putendl_fd("rrr", 1);
+}
