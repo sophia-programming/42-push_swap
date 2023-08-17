fe23a9ed0ba8a16d146c5252c7b979cca282e18d
oaoba <hacchi0803.rera1126@gmail.com>
2023-08-17 13:27:14 +0900

    is_soratedのテストをしたいがmain関数の書き方分からない。

--- src/is_sorted.c
+++ src/is_sorted.c
@@ -14,3 +14,23 @@ bool	is_sorted(t_list **stack)
 	}
 	return (true);
 }
+
+/*これ第二引数に直接整数を入れることはできないとはわかっているけどどうやって
+ * テストしていいか分からず。。
+ */
+int main()
+{
+	t_list *stack = NULL;
+
+	push(&stack, 3);
+	push(&stack, 6);
+	push(&stack, 1);
+	push(&stack, 7);
+
+	if (is_sorted(&stack))
+	{
+		printf("The list is sorted.\n");
+	}
+	else
+		printf("The list is not sorted.\n");
+}
