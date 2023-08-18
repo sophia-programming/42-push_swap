cccacd21efe4f6cfd50cafc5d9710b5b82c9798c
oaoba <hacchi0803.rera1126@gmail.com>
2023-08-18 21:18:16 +0900

    ヘッダーファイル綺麗にまとめた

--- push_swap.h
+++ push_swap.h
@@ -13,11 +13,11 @@ typedef struct s_list
 }t_list;
 
 void	swap(t_list **stack);
-void	put_error(char *str);
-bool	Check_If_INT(long long num);
 void	sa(t_list **stack_a);
 void	sb(t_list **stack_b);
 void	push(t_list **stack_from, t_list **stack_to);
+void	pa(t_list **stack_a, t_list **stack_b);
+void	pb(t_list **stack_b, t_list **stack_a);
 bool	rotate(t_list **stack);
 void	ra(t_list **stack_a);
 void 	rb(t_list **stack_b);
@@ -26,11 +26,17 @@ bool	reverse_rotate(t_list **stack);
 void	rra(t_list **stack_a);
 void	rrb(t_list **stack_b);
 void	rrr(t_list **stack_a, t_list **stack_b);
+
+bool	is_sorted(t_list **stack);
 void	sort_size3(t_list **stack_a);
 void	sort_size4(t_list **stack_a, t_list **stack_b);
-bool	is_sorted(t_list **stack);
+void	sort_size5(t_list **stack_a, t_list **stack_b);
+
 int		min_of_list(t_list *head);
 int		find_min_in_list (const t_list *list);
 int		get_distance_to_min(t_list **stack, int min);
+
+void	put_error(char *str);
+bool	Check_If_INT(long long num);
 
 #endif
