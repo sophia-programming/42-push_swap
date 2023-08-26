e1590d54656d81f03ec283f757bc5bbf315e2b5e
oaoba <hacchi0803.rera1126@gmail.com>
2023-08-26 16:33:21 +0900

    sort_under_5 test ok

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 void	sort_stack(t_list **stack_a, t_list **stack_b)
 {
 	if (ft_lstsize(stack_a) <= 5)
-		simple_sort(stack_a, stack_b);
+		sort_under_5(stack_a, stack_b);
 	else
 		radix_sort(stack_a, stack_b);
 }
