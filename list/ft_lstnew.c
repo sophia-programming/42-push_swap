244136fe3396adf6ed5c1e7b4800e4919809eb04
oaoba <hacchi0803.rera1126@gmail.com>
2023-09-11 13:54:55 +0900

    forbidden functionを使用していた(calloc)。ft_callocに変更

--- list/ft_lstnew.c
+++ list/ft_lstnew.c
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(int data)
 {
 	t_list	*new;
 
-	new = (t_list *)calloc(1, sizeof(t_list));
+	new = (t_list *)ft_calloc(1, sizeof(t_list));
 	if (!new)
 		return (NULL);
 	new->data = data;
